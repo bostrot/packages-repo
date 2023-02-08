3dee146965e2356b83c17964060aa1af12a1026f
Eric <eric@bostrot.com>
2023-02-08 21:30:01 +0100

    Added remarkable desktop app

---
+++ choco/remarkable-desktop-app/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+$releases = 'https://downloads.remarkable.com/latest/windows'
+
+function global:au_GetLatest {
+     # Get release link from redirect header
+     $headers = Invoke-WebRequest -Uri $releases -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction:SilentlyContinue
+     $url = $headers.Headers['Location']
+     # Get version from link (e.g. reMarkable-3.0.4.675-win32.exe)
+     $version = $url -split '-|.exe' | select -Last 1 -Skip 2
+     return @{ Version = $version; URL32 = $url }
+}
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
